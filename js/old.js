8ec5b204aac47bfec7adc2ba011968f51bd1dea7
Michael Rodriguez <69970540+daviid0109@users.noreply.github.com>
2023-07-25 18:51:10 -0500

    clase 22

---
+++ js/old.js
@@ -0,0 +1,38 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById("Hipodoge")
+    let inputCapipepo = document.getElementById("Capipepo")
+    let inputRatigueya = document.getElementById("Ratigueya")
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
+
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = "Hipodoge"
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = "Capipepo"
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = "Ratigueya"
+    } else {
+        alert("Selecciona una mascota")
+    }
+
+    function seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "Hipodoge"
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = "Capipepo"
+    } else {
+        spanMascotaEnemigo.innerHTML = "Ratigueya"
+    }
+}
+    function aleatorio(min,max){return Math.floor(Math.random()*(max-min+1)+min)}
+    
+window.addEventListener("load", iniciarJuego)
